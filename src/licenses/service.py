570002db31b3839c10f1405b5546bdfc1f91e78e
Cristian Ticu <cristian.ticu@eggnita.com>
2024-11-05 13:31:03 +0200

    Fix existing license duplication

--- src/licenses/service.py
+++ src/licenses/service.py
@@ -18,6 +18,14 @@ class LicensesService():
         return [license for license in self._licenses.read_all() if license.user_id == user_id]
 
     def create(self, *, game_id: str, user_id: str):
+        existing_licenses = self.get_all_for_user(user_id=user_id)
+
+        existing_license = [
+            _license for _license in existing_licenses if _license.game_id == game_id]
+
+        if len(existing_license) > 0:
+            return existing_license[0]
+
         license = License(id=str(uuid4()), user_id=user_id,
                           game_id=game_id, acquisition=date.today())
 
